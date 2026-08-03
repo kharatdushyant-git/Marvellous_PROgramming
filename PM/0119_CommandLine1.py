de2db70305955d86d9a65f4d2fcbe2b954c0f4af
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-03 23:16:07 +0530

    Python & ML

---
+++ PM/0119_CommandLine1.py
@@ -0,0 +1,9 @@
+print("Enter First Number : ",end = "")
+value1 = int(input())
+
+print("Enter First Number : ",end = "")
+value2 = int(input())
+
+Ans = value1 + value2
+
+print("Answer is : ",Ans)
