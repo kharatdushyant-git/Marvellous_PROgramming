2424a9ec470c5cb87b2b9a1fdd7752c981abda64
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-10 15:40:41 +0530

    Add 0134_Scripting

---
+++ PM/0134_Scripting.py
@@ -0,0 +1,13 @@
+print("Enter Fisrt Number : ",end="")
+No1= int(input())
+
+print("Enter Second Number : ",end="")
+No2= int(input())
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
+
+Ans = No1 - No2
+
+print("Substraction is : ",Ans)
