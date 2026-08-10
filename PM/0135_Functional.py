cdb7012ea6c510fd9bc9d2b22e3d24dcbc7709ff
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-10 15:40:50 +0530

    Add 0135_Functional

---
+++ PM/0135_Functional.py
@@ -0,0 +1,15 @@
+Add = lambda No1, No2 : No1 + No2
+
+Sub = lambda No1, No2 : No1 - No2
+
+print("Enter Fisrt Number : ",end="")
+value1= int(input())
+
+print("Enter Second Number : ",end="")
+value2 = int(input())   
+
+Ret = Add(value1,value2)
+print("Addition is : ",Ret) 
+
+Ret = Sub(value1,value2)
+print("Substraction is : ",Ret) 
