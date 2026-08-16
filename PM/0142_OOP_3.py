81ca139189b300f66838ab0115d65b2f2798952b
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-16 22:35:16 +0530

    Add 0142_OOP_3

---
+++ PM/0142_OOP_3.py
@@ -0,0 +1,31 @@
+class Demo:
+    #class Variable
+    value1 = 10
+    value2 = 20
+
+    def __init__(self):
+        self.No1 = 11
+        self.No2 = 21
+
+    #instance method
+    def fun(self):
+        print("Inside Instance method named as fun : ")
+
+        print("class Variable : ",self.No1)
+        print("class variable : ",self.No2)
+
+        print("instance variable : ",Demo.value1)
+        print("instance variable : ",Demo.value2)
+
+    @classmethod
+    def gun(cls):
+        print("Inside Instance method named as gun : ")
+
+        #print("class Variable : ",Demo.No1)           Not Allowed
+        #print("class variable : ",Demo.No2)           Not Allowed
+
+        print("instance variable : ",Demo.value1)
+        print("instance variable : ",Demo.value2)
+
+#Call Without Object
+Demo.gun()     
