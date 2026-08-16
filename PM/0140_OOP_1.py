4f5ee695d5955afe04a90b1127a6311171306d3c
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-16 22:35:04 +0530

    Add 0140_OOP_1

---
+++ PM/0140_OOP_1.py
@@ -0,0 +1,21 @@
+class Demo:
+    #class Variable
+    value1 = 10
+    value2 = 20
+
+    def __init__(self):
+        self.No1 = 11
+        self.No2 = 21
+
+    #instance method
+    def fun(self):
+        print("Inside Instance method named as fun : ")
+
+        print("class Variable : ",self.No1)
+        print("class variable : ",self.No2)
+
+        print("instance variable : ",self.value1)
+        print("instance variable : ",self.value2)
+
+dobj = Demo()
+dobj.fun      
