ea49b1f58bb60bb291e45a86010e45547f748679
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-17 22:04:58 +0530

    Addded 0145_OOP_5.py

---
+++ PM/0145_OOP_5.py
@@ -0,0 +1,32 @@
+class Demo:
+    #class Variable
+    value1 = 10
+    value2 = 20
+
+    def __init__(self):
+        self.No1 = 11
+        self.No2 = 21
+
+    #instance method
+    def fun(self):
+        print("Inside Instance method named as fun : ")
+
+        print("class Variable : ",self.No1)
+        print("class variable : ",self.No2)
+
+        print("instance variable : ",Demo.value1)
+        print("instance variable : ",Demo.value2)
+
+    @classmethod
+    def gun(cls):
+        print("Inside Instance method named as gun : ")
+
+        #print("class Variable : ",Demo.No1)           Not Allowed
+        #print("class variable : ",Demo.No2)           Not Allowed
+
+        print("instance variable : ",cls.value1)
+        print("instance variable : ",cls.value2)
+
+#Call With Object
+dobj = Demo()
+dobj.gun()     
