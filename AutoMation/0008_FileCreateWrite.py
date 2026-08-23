55652eb25b594be784fb91e56949b1cf88873ad7
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-24 01:20:50 +0530

    Add 0008_FileCreateWrite.py

---
+++ AutoMation/0008_FileCreateWrite.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        fobj = open("Demo.txt","w")
+
+        print("File gets opened !!!!")
+
+        fobj.close()
+
+    except FileNotFoundError as fobj:
+
+        print("File is not present current Directory")   
+
+if __name__ == "__main__":
+    main()
