98f51db3c569d814bfb4a582ec76f9b70fc6fb46
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-24 01:19:51 +0530

    Add 0006_FileOpenException.py

---
+++ AutoMation/0006_FileOpenException.py
@@ -0,0 +1,12 @@
+def main():
+    try:
+        open("Demo.txt","r")
+
+        print("File gets opened !!!!")
+
+    except FileNotFoundError as fobj:
+
+        print("File is not present current Directory")   
+
+if __name__ == "__main__":
+    main()
