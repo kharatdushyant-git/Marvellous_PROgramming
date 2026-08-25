0a3b47a6a4c5b96bf529a8c95c603bdc31eef949
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-08-25 23:39:19 +0530

    Add 0015_FileReadTellX.py

---
+++ AutoMation/0015_FileReadTellX.py
@@ -0,0 +1,28 @@
+def main():
+    try:
+        fobj = open("Demo.txt","r")
+
+        print("File gets opened !!!!")
+
+        print("File offset is : ",fobj.tell())
+
+        data = fobj.read(10)
+
+        print(data)                             # 10
+
+        print("File offset is : ",fobj.tell())
+
+        data = fobj.read(10)
+
+        print(data)                             # 20
+
+        print("File offset is : ",fobj.tell())
+
+        fobj.close()
+
+    except FileNotFoundError as fobj:
+
+        print("File is not present current Directory")   
+
+if __name__ == "__main__":
+    main()
