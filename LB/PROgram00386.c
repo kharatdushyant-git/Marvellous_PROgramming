5e042792bb839b7322f9928c6f77855cfe394825
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-04 22:48:57 +0530

    PROgram00386.c

---
+++ LB/PROgram00386.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+# pragma pack (1)
+struct node
+{
+    int data;
+    struct node * next;
+    struct node * prev;            //$
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void Display(PNODE fist)
+{
+
+}
+
+int Count(PNODE first)
+{
+    return 0;
+}
+
+void InsertFirst(PPNODE first, int iNo)
+{
+
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    
+}
+
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{
+
+}
+
+void DeleteFirst(PPNODE first)
+{
+
+}
+
+void DeleteLast(PPNODE first)
+{
+    
+}
+
+void DeleteAtPos(PPNODE first, int iNo,int iPos)
+{
+
+}
+
+int main()
+{
+    PNODE head = NULL;
+    return 0;
+}
