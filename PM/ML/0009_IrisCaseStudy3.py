353e76309c1001ee9d1ae61692cd39bda51b841b
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-06 17:38:06 +0530

    Add 0009_IrisCaseStudy3.py

---
+++ PM/ML/0009_IrisCaseStudy3.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+
+def main():
+    print("-"*30)
+    print("Iris Classification case study")
+    print("-"*30)
+
+    Dataset = load_iris()
+
+    print(Dataset)
+
+    # metadata of data set
+    print("Independent variable are : ")
+    print(Dataset.feature_names)
+    print("Length of independent variable : ",len(Dataset.feature_names))
+
+    print("Dependent variable are : ")
+    print(Dataset.target_names)
+    print("Length of independent variable : ",len(Dataset.target_names))
+    
+
+if __name__ == "__main__":
+    main()    
