7dbd6e03df9aad73e05ca88f44983e981bb8df4f
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-12 10:13:23 +0530

    Add PM/AutoMation/00024_AutomationScript2.py

---
+++ PM/AutoMation/00024_AutomationScript2.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+
+    if(len(sys.argv) == 2):
+        DirectoryName = sys.argv[1]
+        print("Directory Name is : ",DirectoryName)
+    else:
+        print("invalid Number of Arguments")
+
+if __name__ == "__main__":
+    main()
