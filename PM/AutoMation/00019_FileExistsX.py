a08ff7fc03bbd22a25f4273e1074ca6c3aa1e8fc
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-12 10:11:22 +0530

    Add PM/AutoMation/00019_FileExistsX.py

---
+++ PM/AutoMation/00019_FileExistsX.py
@@ -0,0 +1,13 @@
+import os
+
+def main():
+    ret = os.path.exists("Demo.txt")
+
+    if (ret == True):
+        print("File is Present in Current directory")
+
+    else:
+        print("There is no such File")    
+
+if __name__ == "__main__":
+    main()
