173739d3e65b205ea158a662ba2a3ea1b0d9e871
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-17 21:40:59 +0530

    Add 173_SpecificClass.cpp

---
+++ PPA/173_SpecificClass.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Arithematic
+{
+    public:
+        int No1;
+        int No2;
+
+        Arithematic(int A, int B)
+        {
+            this->No1 = A;
+            this->No2 = B;
+        }
+
+        int Addition()
+        {
+            int Ans;
+            Ans = No1 + No2;
+            return Ans;
+        }
+
+        int Substraction()
+        {
+            int Ans;
+            Ans = No1 - No2;
+            return Ans;
+        }
+};
+
+int main()
+{
+    Arithematic aobj(11,10);
+
+    cout<<aobj.Addition()<<"\n";            // 21
+    cout<<aobj.Substraction()<<"\n";        // 1
+    
+    return 0;
+}
