bc10bb51a14426cb6ea013d5e0538e1708b8848f
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-19 19:12:00 +0530

    Add 00029_DirectoryScanner-1.py

---
+++ PM/AutoMation/00029_DirectoryScanner-1.py
@@ -0,0 +1,43 @@
+import sys
+import os
+
+def DirectoryScanner(DirectoryPath):
+    
+    print("Files From the Directory are : ")
+    
+    for FolderName, SubFolder, FileName in os.walk(DirectoryPath):
+        for fname in FileName:
+            print(fname)
+
+def main():
+
+    Border = "-"*40
+    print(Border)
+    print(" Marvellous Automation Script")
+    print(Border)
+
+    if(len(sys.argv) == 2):
+    
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This automation script is used to travel the directory")
+            print("For better usage please check --u flag")
+
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Please Execute the script as ")
+            print("Python Filename.py DirectoryName")
+            print("DirectoryName should be Absolute path")
+
+        else:
+            DirectoryScanner(sys.argv[1])
+
+    else:
+        print("invalid Number of Arguments")
+        print("Please use --h or --u for more information")
+
+    print("\n")
+    print(Border)    
+    print(" Thanks for using Marvellous Automation Script")
+    print(Border)    
+
+if __name__ == "__main__":
+    main()
