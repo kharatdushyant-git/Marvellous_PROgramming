7488c87d192ddb7dbba805d1bad47be1942b73da
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-19 19:11:20 +0530

    Add 00027_AutomationScript5.py

---
+++ PM/AutoMation/00027_AutomationScript5.py
@@ -0,0 +1,34 @@
+import sys
+
+def main():
+
+    print("------------------------------------------------------------------")
+    print(" Marvellous Automation Script")
+    print("------------------------------------------------------------------")
+
+
+    if(len(sys.argv) == 2):
+        
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This automation script is used to travel the directory")
+            print("For better usage please check --u flag")
+
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Please Execute the script as ")
+            print("Python Filename.py DirectoryName")
+            print("DirectoryName should be Absolute path")
+
+        else:
+            DirectoryName = sys.argv[1]
+            print("Directory Name is : ",DirectoryName)
+
+    else:
+        print("invalid Number of Arguments")
+        print("Please use --h and --u for more information")
+
+    print("------------------------------------------------------------------")
+    print(" Thanks for using Marvellous Automation Script")
+    print("------------------------------------------------------------------")    
+
+if __name__ == "__main__":
+    main()
