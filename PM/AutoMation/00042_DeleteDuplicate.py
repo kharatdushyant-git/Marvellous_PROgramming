e988ccae572d19027c81c78fdaacadacfce644af
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-20 14:10:15 +0530

    Add 00042_DeleteDuplicate.py

---
+++ PM/AutoMation/00042_DeleteDuplicate.py
@@ -0,0 +1,79 @@
+import sys
+import os
+import hashlib
+
+def CaculateCheckSum(FileName):
+
+    fobj = open(FileName,"rb") 
+
+    #1
+    hobj = hashlib.md5()
+
+    Buffer = fobj.read(1024)
+
+    while(len(Buffer) > 0):
+        #2
+        hobj.update(Buffer)
+        Buffer = fobj.read(1024)
+
+    fobj.close()    
+    # 3
+    return hobj.hexdigest()
+
+def FindDuplicate(DirectoryName):
+    Ret = False
+
+    Ret = os.path.exists(DirectoryName)
+
+    if Ret == False:
+        print("Path is Invalid")
+        return    
+
+    Ret = os.path.isdir(DirectoryName)
+
+    if Ret == False:
+        print("It's not a Directory")    
+        return
+
+    Duplicate = {}
+
+    Unique = 0
+    Same = 0
+    for FolderName , SubFolder , FileName in os.walk(DirectoryName):
+        
+        for fname in FileName:
+
+            fname = os.path.join( FolderName, fname )   
+
+            CheckSum = CaculateCheckSum(fname)
+
+            if CheckSum in Duplicate:
+                Same = Same + 1
+                Duplicate[CheckSum].append(fname)
+
+            else:
+                Unique = Unique + 1
+                Duplicate[CheckSum] = [fname]
+
+    return Duplicate               
+
+def DeleteDuplicate(DirectoryName):
+    MyDict = FindDuplicate(DirectoryName)
+
+    Result = MyDict.values()
+
+    return Result
+    # Result = list(filter((lambda x : len(x) > 1, MyDict.values())))
+    # Result = list(filter((lambda x : len(x) > 1, MyDict.keys())))
+
+def main():
+    Data1 = DeleteDuplicate("TEST")
+    print(Data1)
+
+    print("\n")
+
+    Data2 = FindDuplicate("TEST")
+    print(Data2)
+
+if __name__ == "__main__":
+    main()
