7b2ab9fa8ca59b8770e0809d70743dcff2cf3c00
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-20 14:09:56 +0530

    Add 00041_DirectoryCheckSumStoreX.py

---
+++ PM/AutoMation/00041_DirectoryCheckSumStoreX.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import hashlib
+
+def CaculateCheckSum(FileName):
+
+    fobj = open(FileName,"rb") 
+
+    #1
+    hobj = hashlib.md5()
+
+    Buffer = fobj.read(1024)
+
+    while(len(Buffer) > 0):
+        #2
+        hobj.update(Buffer)
+        Buffer = fobj.read(1024)
+
+    fobj.close()    
+    # 3
+    return hobj.hexdigest()
+
+def FindDupicate(DirectoryName):
+    Ret = False
+
+    Ret = os.path.exists(DirectoryName)
+
+    if Ret == False:
+        print("Path is Invalid")
+        return    
+
+    Ret = os.path.isdir(DirectoryName)
+
+    if Ret == False:
+        print("It's not a Directory")    
+        return
+
+    Duplicate = {}
+
+    Unique = 0
+    Same = 0
+    for FolderName , SubFolder , FileName in os.walk(DirectoryName):
+        
+        for fname in FileName:
+
+            fname = os.path.join( FolderName, fname )   
+
+            CheckSum = CaculateCheckSum(fname)
+
+            if CheckSum in Duplicate:
+                Same += 1
+                Duplicate[CheckSum].append(fname)
+
+            else:
+                Unique += 1
+                Duplicate[CheckSum] = [fname]
+
+    return Duplicate               
+
+def main():
+    Data = FindDupicate("TEST")
+    print(Data)
+
+
+if __name__ == "__main__":
+    main()
