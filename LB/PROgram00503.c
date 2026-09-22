7b016297d13433ddb3a9c4d096507cbeb3581733
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-22 21:06:28 +0530

    Add PROgram00503.c

---
+++ LB/PROgram00503.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void Display ()
+{
+    
+    print("Jay Ganesh...\n");   
+
+}
+
+int main()
+{
+    Display();
+    Display();
+    Display();
+    Display();
+    Display();
+
+    return 0;
+}
