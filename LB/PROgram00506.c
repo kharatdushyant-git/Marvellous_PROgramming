74717d2ffb82b24aa7830e75a47145b4f31f6d38
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-22 21:09:42 +0530

    Add PROgram00506.c

---
+++ LB/PROgram00506.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+//stack overflow
+void Display ()
+{
+    static int i = 1;
+
+    printf("Jay Ganesh...%d\n",i);
+    i++;
+    
+    Display();
+
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
