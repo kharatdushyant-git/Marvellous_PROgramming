533a2f315b0059afd0de709f2b90773d433f646f
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-23 22:18:25 +0530

    Add PROgram00513.c

---
+++ LB/PROgram00513.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Display( int iNo)
+{
+    auto int i = 0;
+
+    i = 1;
+    while(i <= iNo)
+    {
+        printf("Jay Ganesh... %d\n",i);
+        i++;
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter the Frequency : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
