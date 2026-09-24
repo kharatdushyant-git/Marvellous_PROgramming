bd356775977e189d48a2c16d9064443410613ff3
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-24 22:54:03 +0530

    Add PROgram00527.c

---
+++ LB/PROgram00527.c
@@ -0,0 +1,31 @@
+// 4
+// 4 * 3 * 2 * 1 = 24
+
+#include<stdio.h>
+
+int Factorial(int iNo)
+{
+    int iFact = 1;
+    
+    while(iNo > 0)
+    {
+        iFact = iFact * iNo;
+        iNo --;
+    }
+
+    return iFact;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+    
+    printf("Enter the Frequency : ");
+    scanf("%d",&iValue);
+
+    iRet = Factorial(iValue);
+
+    printf("Factorial is : %d",iRet);
+
+    return 0;
+}
