f2e6d473c6f5c2f5018110ea66585aae19d13add
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-25 19:24:19 +0530

    Add PROgram00534.c

---
+++ LB/PROgram00534.c
@@ -0,0 +1,33 @@
+// 5678
+// 5 6 7 8
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iDigit = 0;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        
+        iNo = iNo / 10;
+
+        Display(iNo);
+
+        printf("%d\n",iDigit);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
