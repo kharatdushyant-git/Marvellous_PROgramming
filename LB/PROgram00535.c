24f9397e75d569e50979aabfdf6444f5c4388208
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-25 19:24:30 +0530

    Add PROgram00535.c

---
+++ LB/PROgram00535.c
@@ -0,0 +1,36 @@
+// 5678
+// 8 + 7 + 6 + 5 = 26
+
+#include<stdio.h>
+
+int Summation(int iNo)
+{
+    int iDigit = 0;
+    static int iSum = 0;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iSum = iSum + iDigit;
+        iNo = iNo / 10;
+
+        Summation(iNo);
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    iRet = Summation(iValue);
+
+    printf("Summaiton of digits is : %d",iRet);
+
+    return 0;
+}
