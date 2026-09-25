a0682b4661aecd25570d6d5b22faf9fbdefabfd4
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-25 19:24:57 +0530

    Add PROgram00537.c

---
+++ LB/PROgram00537.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int Summation(int iNo)
+{
+    int iDigit = 0;
+    static int iSum = 1;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iSum = iSum * iDigit;
+        iNo = iNo / 10;
+
+        Summation(iNo);
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    iRet = Summation(iValue);
+
+    printf("Multiplication of digits is : %d",iRet);
+
+    return 0;
+}
